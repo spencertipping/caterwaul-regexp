6ae20ff66bfd94c7e1ad1d96f74a28ad8abff3a5
Spencer Tipping <spencer@spencertipping.com>
2011-08-31 20:10:37 -0600

    Wrote a probably-broken combinatory parser for regexp syntax.

--- regexp.js
+++ regexp.js
@@ -1,66 +1,66 @@
-Caterwaul regular expression parser | Spencer Tipping
-Licensed under the terms of the MIT source code license
+// Caterwaul regular expression parser | Spencer Tipping
+// Licensed under the terms of the MIT source code license
 
-Introduction.
-This library parses regular expressions into syntax trees. The resulting trees not only describe the structure of the regular expression but also provide information about the strings that it
-matches. It knows all of the standard Javascript regular expression constructs such as groups, backreferences, lookahead, etc.
+// Introduction.
+// This library parses regular expressions into syntax trees. The resulting trees not only describe the structure of the regular expression but also provide information about the strings that it
+// matches. It knows all of the standard Javascript regular expression constructs such as groups, backreferences, lookahead, etc.
 
-Implementation.
-Regular expressions support three binary operators. One is the dash, used for character ranges; another is the pipe, which is used for alternatives; and the third is the implicit join
-operator, used wherever two constructs are adjacent. The pipe has lower precedence than implicit joining, and the dash has higher precedence (though it's not exactly comparable, since it's
-special only inside a character class and implicit join does something different there).
+// Implementation.
+// Regular expressions support three binary operators. One is the dash, used for character ranges; another is the pipe, which is used for alternatives; and the third is the implicit join
+// operator, used wherever two constructs are adjacent. The pipe has lower precedence than implicit joining, and the dash has higher precedence (though it's not exactly comparable, since it's
+// special only inside a character class and implicit join does something different there).
 
-This parser interprets regular expressions a little differently than their syntax might suggest. For example:
+// This parser interprets regular expressions a little differently than their syntax might suggest. For example:
 
-| caterwaul.regexp(/foo(?=bar)/).structure()    // -> '("," foo ("(?=" bar))'
-  caterwaul.regexp(/foo(?:bar)+/).structure()   // -> '("," foo ("+" ("(?:" bar)))'
-  caterwaul.regexp(/[a-z]/).structure()         // -> '("[" ("-" a z))'
-  caterwaul.regexp(/[^a-z]/).structure()        // -> '("[^" ("-" a z))'
-  caterwaul.regexp(/[a-zABC]/).structure()      // -> '("[" ("," ("-" a z) ("," A ("," B C))))
-  caterwaul.regexp(/[-abc]/).structure()        // -> '("[" ("," - ("," a ("," b c))))
+// | caterwaul.regexp(/foo(?=bar)/).structure()    // -> '("," foo ("(?=" bar))'
+//   caterwaul.regexp(/foo(?:bar)+/).structure()   // -> '("," foo ("+" ("(?:" bar)))'
+//   caterwaul.regexp(/[a-z]/).structure()         // -> '("[" ("-" a z))'
+//   caterwaul.regexp(/[^a-z]/).structure()        // -> '("[^" ("-" a z))'
+//   caterwaul.regexp(/[a-zABC]/).structure()      // -> '("[" ("," ("-" a z) ("," A ("," B C))))
+//   caterwaul.regexp(/[-abc]/).structure()        // -> '("[" ("," - ("," a ("," b c))))
 
-These syntax trees aren't instances of caterwaul.syntax. Rather, they're instances of caterwaul.regexp.syntax. Normally this isn't an important distinction, since they generally support the
-same set of methods. (All tree nodes inherit methods from caterwaul.syntax_common.) The only case where it really matters is that you get regexp-specific methods for these trees:
+// These syntax trees aren't instances of caterwaul.syntax. Rather, they're instances of caterwaul.regexp.syntax. Normally this isn't an important distinction, since they generally support the
+// same set of methods. (All tree nodes inherit methods from caterwaul.syntax_common.) The only case where it really matters is that you get regexp-specific methods for these trees:
 
-| caterwaul.regexp(/foo/).example()             // -> 'foo'
-  caterwaul.regexp(/foo/).cardinality()         // -> 1
-  caterwaul.regexp(/foo/).minimum_length()      // -> 3
-  caterwaul.regexp(/foo/).maximum_length()      // -> 3
-  caterwaul.regexp(/foo/).match_groups()        // -> 0
+// | caterwaul.regexp(/foo/).example()             // -> 'foo'
+//   caterwaul.regexp(/foo/).cardinality()         // -> 1
+//   caterwaul.regexp(/foo/).minimum_length()      // -> 3
+//   caterwaul.regexp(/foo/).maximum_length()      // -> 3
+//   caterwaul.regexp(/foo/).match_groups()        // -> 0
 
-The minimum_length and maximum_length methods may not be useful in some cases. For example, caterwaul.regexp(/$/).maximum_length() returns Infinity, indicating that there is no string that
-will be so long that it fails to match. example() can also fail, and it will do so by throwing an error if no string can be found. For instance, caterwaul.regexp(/foo^bar/).example() complains
-that the ^ assertion can't be met because it has preceding characters and has no way to remove them. However, caterwaul.regexp(/(foo)?^bar/).example() happily returns 'bar'.
+// The minimum_length and maximum_length methods may not be useful in some cases. For example, caterwaul.regexp(/$/).maximum_length() returns Infinity, indicating that there is no string that
+// will be so long that it fails to match. example() can also fail, and it will do so by throwing an error if no string can be found. For instance, caterwaul.regexp(/foo^bar/).example() complains
+// that the ^ assertion can't be met because it has preceding characters and has no way to remove them. However, caterwaul.regexp(/(foo)?^bar/).example() happily returns 'bar'.
 
-There are some cases where example() erroneously fails. These have to do with weird ways in which you can use backreferences with lookaheads, e.g.
-caterwaul.regexp(/(foo)?bar(?=foo)\1/).example() complains because it won't revisit its initial assignment of (foo)? to the empty string. I consider this an acceptable failure, since the
-(foo)? term isn't really optional. Because nobody would do this in practice, I'm going to leave it this way. (This problem also affects minimum_length() and maximum_length().)
+// There are some cases where example() erroneously fails. These have to do with weird ways in which you can use backreferences with lookaheads, e.g.
+// caterwaul.regexp(/(foo)?bar(?=foo)\1/).example() complains because it won't revisit its initial assignment of (foo)? to the empty string. I consider this an acceptable failure, since the
+// (foo)? term isn't really optional. Because nobody would do this in practice, I'm going to leave it this way. (This problem also affects minimum_length() and maximum_length().)
 
-Syntax trees generated by caterwaul.regexp() know some things about the environment in which they were evaluated. For example:
+// Syntax trees generated by caterwaul.regexp() know some things about the environment in which they were evaluated. For example:
 
-| caterwaul.regexp(/foo/i).i()                  // -> true
-  caterwaul.regexp(/^foo/m).m()                 // -> true
+// | caterwaul.regexp(/foo/i).i()                  // -> true
+//   caterwaul.regexp(/^foo/m).m()                 // -> true
 
-Because these trees are still normal Caterwaul syntax trees, you can do all of the usual matching stuff:
+// Because these trees are still normal Caterwaul syntax trees, you can do all of the usual matching stuff:
 
-| caterwaul.regexp(/_a*/).match(caterwaul.regexp(/xs*/))        // -> {_a: xs, _: xs*}
+// | caterwaul.regexp(/_a*/).match(caterwaul.regexp(/xs*/))        // -> {_a: xs, _: xs*}
 
-Supported syntax.
-This library parses the following constructs (obtained from the Mozilla regular expression documentation: https://developer.mozilla.org/en/JavaScript/Guide/Regular_Expressions):
+// Supported syntax.
+// This library parses the following constructs (obtained from the Mozilla regular expression documentation: https://developer.mozilla.org/en/JavaScript/Guide/Regular_Expressions):
 
-| 1. Atoms                      e.g. /foo/, /bar./
-  2. Zero-width assertions      e.g. /^/, /$/, /\b/, /\B/
-  3. Repetition                 e.g. /a+/, /b*/, /a+?/, /b*?/, /c{10,20}/, /c{10}/, /c{10,}/, /c{10,20}?/, /c{10,}?/, /d?/
-  4. Capturing                  e.g. /a(.)c/
-  5. Non-capturing              e.g. /a(?:.)c/
-  6. Lookahead                  e.g. /a(?=b)/, /a(?!b)/
-  7. Disjunction                e.g. /a|b/
-  8. Character classes          e.g. /[abc]/, /[^abc]/, /[a-df]/
-  9. Special characters         e.g. /\w\W\s\S\d\D\f\n\r\t\v\0\xff\uffff/
- 10. Backreferences             e.g. /(a)\1/
+// | 1. Atoms                      e.g. /foo/, /bar./
+//   2. Zero-width assertions      e.g. /^/, /$/, /\b/, /\B/
+//   3. Repetition                 e.g. /a+/, /b*/, /a+?/, /b*?/, /c{10,20}/, /c{10}/, /c{10,}/, /c{10,20}?/, /c{10,}?/, /d?/
+//   4. Capturing                  e.g. /a(.)c/
+//   5. Non-capturing              e.g. /a(?:.)c/
+//   6. Lookahead                  e.g. /a(?=b)/, /a(?!b)/
+//   7. Disjunction                e.g. /a|b/
+//   8. Character classes          e.g. /[abc]/, /[^abc]/, /[a-df]/
+//   9. Special characters         e.g. /\w\W\s\S\d\D\f\n\r\t\v\0\xff\uffff/
+//  10. Backreferences             e.g. /(a)\1/
 
-Backreferences have a peculiarity that I wasn't previously aware of. If there are more than ten match groups at the current parse point, then backreferences are parsed as potentially having
-two digits. Presumably the engine is insightful enough to discard digits that would forms numbers larger than the number of seen match groups, and this parser does that.
+// Backreferences have a peculiarity that I wasn't previously aware of. If there are more than ten match groups at the current parse point, then backreferences are parsed as potentially having
+// two digits. Presumably the engine is insightful enough to discard digits that would forms numbers larger than the number of seen match groups, and this parser does that.
 
 caterwaul.js_all()(function ($) {
   $.regexp(r)     = $.regexp.parse.apply(this, arguments),
@@ -145,3 +145,5 @@ caterwaul.js_all()(function ($) {
                                                                   base               = alt(positive_lookahead, negative_lookahead, forgetful_group, group,
                                                                                            character_not_in, character_in, zero_width, escaped, escaped_slash,
                                                                                            control, hex_code, unicode, backreference, dot, word, other)]]]})(caterwaul);
+
+// Generated by SDoc 
